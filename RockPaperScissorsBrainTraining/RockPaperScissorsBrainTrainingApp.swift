28af7e49fda34deca453e1c61e0d4457c663c4bb
stevegustason <steve.gustason@gmail.com>
2023-03-25 18:53:19 -0500

    Initial Commit

---
+++ RockPaperScissorsBrainTraining/RockPaperScissorsBrainTrainingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsBrainTrainingApp.swift
+//  RockPaperScissorsBrainTraining
+//
+//  Created by Steven Gustason on 3/25/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsBrainTrainingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
